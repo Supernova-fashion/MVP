90de8bc0a66066a1de0832f3ef0c37d672209356
Vladislav Ainshtein <ainshtein.v@liceum1535.ru>
2022-10-10 23:23:36 +0300

    public ip added to allowed hosts

--- Supernova/settings.py
+++ Supernova/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1zm+x+u4car5du!2j%uh)ls1wfko829ox!vt(vfhu^3l%g!95!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.1', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.1', '127.0.0.1', '213.87.11.126']
 
 
 # Application definition
